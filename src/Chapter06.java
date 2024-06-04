0913d0ba29304df1924dbb912eed3017d17f8b86
NobuteruKumai <171550188+Chaason@users.noreply.github.com>
2024-06-04 12:39:11 +0900

    Chapter6の確認問題

---
+++ src/Chapter06.java
@@ -0,0 +1,35 @@
+
+public class Chapter06 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		//long型の変数に10000000000を代入して出力
+		long num1 = 10000000000L;
+		System.out.println(num1);
+		
+		//double型の変数に3.14を代入して出力
+		double num2 = 3.14;
+		System.out.println(num2);
+		
+		//boolean型の変数にtrueを代入して出力
+		boolean a = true ;
+		System.out.println(a);
+		
+		//Integer型の変数に100を代入して出力
+		Integer intNum = 100;
+		System.out.println(intNum);
+		
+		//Float型の変数に1.4142を代入して出力
+		Float floatNum = 1.4142f;
+		System.out.println(floatNum);
+		
+		//Character型の変数に「う」を代入して出力
+		Character chara = 'う';
+		System.out.println(chara);
+		
+		//要素数が５のint配列に「0,10,20,30,40」を代入してインデックス番号が３の値を出力
+		int[]number = {0,10,20,30,40};
+		System.out.println(number[3]);
+	}
+
+}
