df904eb77d8acd3540c387b6049990127b47dee2
NobuteruKumai <171550188+Chaason@users.noreply.github.com>
2024-06-04 13:20:34 +0900

    L2C8のサンプルコード

---
+++ src/if01.java
@@ -0,0 +1,18 @@
+
+public class if01 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int number = 7;
+		if(number < 5 ) {
+			System.out.println(number + "は５未満です");
+		}
+		if(number < 10) {
+			System.out.println(number + "は１０未満です");
+		}
+		if(number < 15) {
+			System.out.println(number + "は１５未満です");
+		}
+	}
+
+}
