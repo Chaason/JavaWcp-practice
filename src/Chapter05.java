257a9fca545dd477dd19215295dd46664b505d97
NobuteruKumai <171550188+Chaason@users.noreply.github.com>
2024-06-04 12:06:46 +0900

    Chapter05.javaとVariable01.javaの追加

---
+++ src/Chapter05.java
@@ -0,0 +1,13 @@
+
+public class Chapter05 {
+	public static void main(String[]args) {
+		String name = "Nobuteru Kumai";
+		System.out.println(name);
+		
+		name = "Have a good day!";
+		System.out.println(name);
+		
+		final int luckyNumber = 5;
+		System.out.println(luckyNumber);
+	}
+}
